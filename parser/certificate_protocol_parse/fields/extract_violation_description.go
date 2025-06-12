7475fc05eb7603c1bc1e15a300e630eafeff1c1a
Dragomir Parvanov <bieltdrago@gmail.com>
2025-06-12 17:45:39 +0300

    refactor: fixed lint errors

--- parser/certificate_protocol_parse/fields/extract_violation_description.go
+++ parser/certificate_protocol_parse/fields/extract_violation_description.go
@@ -1,12 +1,11 @@
 package certificate_protocol_parse_fields
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 )
 
 func ExtractViolationDescription(section *goquery.Selection) string {
-	contains := fmt.Sprintf("td:contains('При сечта и извоза на дървесина са допуснати следните нарушения на Закона за горите:')")
+	contains := "td:contains('При сечта и извоза на дървесина са допуснати следните нарушения на Закона за горите:')"
 
 	line := section.Find(contains).First().Find("b").Next().Text()
 
--- parser/certificate_protocol_parse/fields/extract_violation_recipient.go
+++ parser/certificate_protocol_parse/fields/extract_violation_recipient.go
@@ -1,12 +1,11 @@
 package certificate_protocol_parse_fields
 
 import (
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
 )
 
 func ExtractViolationRecipient(section *goquery.Selection) string {
-	contains := fmt.Sprintf("td:contains('За допуснатите нарушения е съставен акт за установяване на административно')")
+	contains := "td:contains('За допуснатите нарушения е съставен акт за установяване на административно')"
 
 	line := section.Find(contains).First().Find("b").Next().Next().Next().Next().Text()
 
