c20e336c914209e5c267573af36d3d887014a174
Dragomir Parvanov <bieltdrago@gmail.com>
2025-06-16 15:38:49 +0300

    refactor: oops, its regions, not municipalities

---
+++ parser/regions.go
@@ -0,0 +1,37 @@
+package parser
+
+type Region int32
+
+/*
+Where the value is the id corresponding in the iag website
+*/
+const (
+	Blagoevgrad   Region = 1
+	Burgas        Region = 2
+	Varna         Region = 3
+	VelikoTarnovo Region = 4
+	Vidin         Region = 5
+	Vratsa        Region = 6
+	Gabrovo       Region = 7
+	Dobrich       Region = 8
+	Kardzhali     Region = 9
+	Kyustendil    Region = 10
+	Lovech        Region = 11
+	Montana       Region = 12
+	Pazardzhik    Region = 13
+	Pernik        Region = 14
+	Pleven        Region = 15
+	Plovdiv       Region = 16
+	Razgrad       Region = 17
+	Ruse          Region = 18
+	Silistra      Region = 19
+	Sliven        Region = 20
+	Smolyan       Region = 21
+	SofiaCity     Region = 22
+	SofiaRegion   Region = 23
+	StaraZagora   Region = 24
+	Targovishte   Region = 25
+	Haskovo       Region = 26
+	Shumen        Region = 27
+	Yambol        Region = 28
+)
