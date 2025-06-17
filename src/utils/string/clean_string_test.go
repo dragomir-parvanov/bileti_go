8c8805f3e98340104703aa2b7e49292f023728e6
Dragomir Parvanov <bieltdrago@gmail.com>
2025-06-17 18:44:29 +0300

    test: added a test

--- src/utils/string/clean_string_test.go
+++ src/utils/string/clean_string_test.go
@@ -1,1 +1,15 @@
 package utils_string
+
+import "testing"
+
+func TestShouldReturnEmptyStringIfItContainsOnlyDots(t *testing.T) {
+	str := "......."
+
+	actual := CleanString(str)
+
+	expected := ""
+
+	if actual != expected {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
